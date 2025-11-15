1d5de126ef84414c25c9c1a6b7e36c777a30e83d
Rodrigo Ruan <rodrigoruan16@hotmail.com>
2025-11-15 13:19:46 -0300

    Adiciona primeira rota para criar usuário

--- server/index.js
+++ server/index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
+app.use(express.json()); // Permite receber dados no formato JSON nas requisições
+
+app.get("/", (_req, res) => {
 	res.send("Hello World!");
+});
+
+app.post("/user/create", (req, res) => {
+	const { username, email, password } = req.body;
+
+	res.status(200).send("OK");
 });
 
 app.listen(port, () => {
